92447d907c07a68c83caa3ce83df93b43b8652ef
Kalina Rząsowska <kalina.rzasowska@comarch.pl>
2022-01-05 10:49:51 +0100

    day5

---
+++ FS/day5/day5.fsx
@@ -0,0 +1,50 @@
+#load "../fileReader.fsx"
+
+open AOC.AOCFileReader
+
+let fileLineToPointPairs = fun (l:string) ->
+     let pairArr = l.Split(" -> ") |> Array.map ( fun p -> 
+        let pointArr = p.Split(",") |> Array.map (fun str -> str |> int)
+        (pointArr.[0], pointArr.[1])
+     )
+     (pairArr.[0], pairArr.[1])
+
+let filterHorizontalOrVerticalLines = fun pairs ->
+    pairs |> Array.filter (fun p ->
+        let (x,y) = fst p
+        let (x2,y2) = snd p
+        x = x2 || y = y2
+    )
+    
+let rec pointPairToPoints = fun ((x1,y1), (x2,y2)) cx cy (points:List<(int * int)>) ->
+    if cx = x2 && cy = y2 then points 
+    else 
+        let nx = if cx < x2 then (cx + 1) else if cx <> x2 then (cx - 1) else cx
+        let ny = if cy < y2 then (cy + 1) else if cy <> y2 then (cy - 1) else cy
+        let newPoints = 
+            if cx = x1 && cy = y1 
+            then List.append points [(x1,y1); (nx,ny)] 
+            else List.append points [(nx,ny)]
+        pointPairToPoints ((x1,y1), (x2,y2)) nx ny newPoints
+
+let getPointsList = fun points ->
+    List.concat (points |> Array.map ( fun p -> 
+        pointPairToPoints p (fst (fst p)) (snd (fst p)) List.empty
+    ))
+
+let groupPoints = fun points ->
+    points |> List.groupBy id |> List.map (fun (c, cs) -> c, List.length cs)
+
+let filterGroupsByLength = fun groups number ->
+    (groups |> List.filter (fun p -> snd p > number)).Length
+
+//1.
+let pointPairs = readFileAsStringLines "./FS/day5/input.txt" |> Array.map fileLineToPointPairs
+let pointsGrouped = filterHorizontalOrVerticalLines pointPairs |> getPointsList |> groupPoints
+let result = filterGroupsByLength pointsGrouped 1 
+printfn "%A" result
+
+//2.
+let diagonalPoints = pointPairs |> getPointsList |> groupPoints
+let result2 = filterGroupsByLength diagonalPoints 1
+printfn "%A" result2
