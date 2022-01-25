f5b8ed5d8d14d1b725e27284bcbdd81e9e7ef89d
Kalina Rząsowska <kalina.rzasowska@comarch.pl>
2022-01-25 18:07:54 +0100

    day9 - part2

--- FS/day9/day9.fsx
+++ FS/day9/day9.fsx
@@ -26,3 +26,50 @@ let rec findLowestPoints i j (heightmap: int[][]) (lowestPoints: list<int>) =
 let getRiskSum (lowest:list<int>) = lowest |> List.map (fun l -> l + 1) |> List.sum
 
 let result = getRiskSum (findLowestPoints 0 0 heightmap [])
+printf "%A" result 
+
+// 2.
+let rec getBasinMap i j basinRows (heightmap:int[][]) =
+    if(i < heightmap.Length && j < heightmap.[0].Length) then
+        if(heightmap.[i].[j] < 9 ) 
+        then getBasinMap i (j + 1) (basinRows |> Map.add (i,j) heightmap.[i].[j]) heightmap
+        else getBasinMap i (j + 1) basinRows heightmap
+    else if (i < heightmap.Length) 
+        then getBasinMap (i + 1) 0 basinRows heightmap
+        else basinRows
+
+let getClosestNeigbours i j (list: List<(int * int) * int>) = 
+    list |> List.filter(fun el -> 
+        let (eli, elj) = fst el
+        let idiff = abs (eli - i)
+        let jdiff = abs (elj - j)
+        (idiff = 0 && jdiff = 1) || (idiff = 1 && jdiff = 0)
+    )
+
+let rec getAllNeighbours (elemsToCheck: List<(int * int) * int>) (allElements: List<(int * int) * int>) (result: List<(int * int) * int>) =
+    match elemsToCheck with
+    | head::tail -> 
+        let (i,j) = fst head
+        let neighbours = getClosestNeigbours i j allElements
+        if(neighbours.Length <> 0) 
+        then
+            let checkEl = ((List.append tail neighbours) |> List.distinct) |> List.except result
+            getAllNeighbours checkEl allElements (List.append [head] result)
+        else getAllNeighbours tail allElements (List.append [head] result)
+    | _ -> result
+
+let rec getAllBasins (elemsToCheck: List<(int * int) * int>) (basins: List<List<(int * int) * int>>) =
+    match elemsToCheck with
+    | head:: tail ->
+        let basin = getAllNeighbours [head] tail List.empty
+        let rest = tail |> List.except basin
+        getAllBasins rest (List.append [basin] basins)
+    | _ -> basins
+
+let getLargestBasinsSizeMultiply basinList = 
+    let basins = getAllBasins basinList List.empty
+    basins |> List.map( fun x -> x.Length) |> List.sortDescending |> List.take 3 |> List.reduce (fun a b -> a * b)
+
+let basinList = getBasinMap 0 0 Map.empty heightmap |> Map.toList
+let result2 = getLargestBasinsSizeMultiply basinList
+printf "%A" result2
